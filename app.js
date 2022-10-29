fa582d2d2ad45d5cd26e034ac6c7be13391c4243
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-29 20:17:35 +0600

    handled get request on the tours endpoint

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello form the server side!', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+app.post('api/v1/tours', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
