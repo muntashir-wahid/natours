6c39402b5326252743d0c0f8026893ac2de0a627
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-29 21:07:39 +0600

    setup the post route for a post request on tours endpoint

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  console.log(newId);
+  res.send('done');
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
