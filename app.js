ed111b7cfa2a370e3da8d9255c015908eee3d654
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-12-19 10:50:25 +0600

    separated route handlers from route deffinations

--- app.js
+++ app.js
@@ -1,26 +1,82 @@
+const fs = require('fs');
 const express = require('express');
 
-const morgan = require('morgan');
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
 const app = express();
-
-// ------------------ //
-// Middlewares
-// ----------------- //
-app.use(morgan('dev'));
-
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const testRouteHandler = (req, res) => {
+  res.status(200).json({ app: 'Natours', message: 'Hello from the server!' });
+};
+
+// ---------------------- //
+// Route handlers
+// ---------------------- //
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = +req.params.id;
+
+  const tour = tours.find((tour) => tour.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newTour = req.body;
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+};
+
+const updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here...',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// ---------------------- //
+// All routes
+// ---------------------- //
+app.get('/', testRouteHandler);
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
 });
-
-// Routers
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-
-module.exports = app;
