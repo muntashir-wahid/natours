82cd02468c3870270cb138a6fd98cd59b67a6641
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-12-27 18:11:02 +0600

    refactored code to implenent MVC pattern and implement the CRUD operations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,51 +1,21 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkId = (req, res, next, val) => {
-  if (tours.length <= val) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invaid id',
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  const body = req.body;
-  if (body.name && body.price) {
-    next();
-  }
-
-  return res.status(400).json({
-    status: 'fail',
-    message: 'Invalid request',
-  });
-};
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
-    data: {
-      tours,
-    },
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = +req.params.id;
-
-  const tour = tours.find((tour) => tour.id === id);
-
   res.status(200).json({
     status: 'success',
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
 
@@ -54,9 +24,9 @@ exports.createTour = (req, res) => {
 
   res.status(201).json({
     status: 'success',
-    data: {
-      tour: newTour,
-    },
+    // data: {
+    //   tour: newTour,
+    // },
   });
 };
 
--- server.js
+++ server.js
@@ -17,39 +17,6 @@ mongoose
   })
   .then(() => console.log('Database connected successfully!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour munst have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'Notun dhaka',
-  price: 10000,
-  raring: 1.2,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((error) => {
-    console.log('Error 💥', error);
-  });
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
