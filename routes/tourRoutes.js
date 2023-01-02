f2a2fa8e8f9c97b42aaf538451681b3e5f9d15a5
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-02 14:14:25 -0800

    calculate some stats on tours data with aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,13 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  getTourStats,
 } = require('./../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
